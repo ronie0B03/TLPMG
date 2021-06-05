bb68a2cb311eefd7bbfd9ef20cdde668cf4324c8
perilloArt3159 <artperillo.ictdu@spcf.edu.ph>
2021-06-05 22:19:47 +0800

    :100: Added barcode in inventory add, inventory table and inventory update (#1)

--- dbh.php
+++ dbh.php
@@ -3,10 +3,22 @@ if (!isset($_SESSION)) {
     session_start();
 }
 
-$host = 'localhost';
-$username = 'ronie';
-$password = 'ronie';
-$database = 'tlpmg';
+$production = false;  
+
+if($production)
+{
+    $host = 'localhost';
+    $username = 'ronie';
+    $password = 'ronie';
+    $database = 'tlpmg';
+}
+else 
+{
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'tlpmg';
+}
 
 $mysqli = new mysqli($host, $username, $password, $database) or die(mysqli_error($mysqli));
 
