2de86f503a02471c77901dba910f5c83a6af1f22
perilloArt3159 <artperillo.ictdu@spcf.edu.ph>
2021-06-06 13:42:57 +0800

    :construction: Add Item by Barcode in Transaction (#2)

--- transactions.php
+++ transactions.php
@@ -22,6 +22,15 @@ else{
     $itemCtrl = $_GET['itemCtrl'];
 }
 
+
+if(!isset($_GET['itemBarCodeCtrl']))
+{
+    $itemBarCodeCtrl = 0;
+}
+else{
+    $itemBarCodeCtrl = $_GET['itemBarCodeCtrl'];
+}
+
 ?>
 <title>Transactions - Celine & Peter Store</title>
 <!-- Content Wrapper -->
@@ -50,6 +59,15 @@ else{
                     ?>
                 </div>
             <?php } ?>
+            <?php if (isset($_SESSION['errors'])): ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    <?php
+                    echo $_SESSION['errors'];
+                    unset($_SESSION['errors']);
+                    ?>
+                </div>
+            <?php endif ?>
             <!-- End Alert here -->
 
             <!-- Add Transaction -->
@@ -60,96 +78,164 @@ else{
                 <div class="card-body">
                     <div class="table-responsive">
                         <form method="post" action="process_transaction.php">
-
                             <table class="table">
                                 <thead>
                                     <tr>
-                                        <th>Item</th>
-                                        <th>Quantity</th>
-                                        <th>Adjusted Price / Item</th>
-                                        <th>Subtotal</th>
+                                        <th width="45%">Barcode</th>
+                                        <th width="25%">Quantity</th>
+                                        <th width="15%">Price</th>
+                                        <th width="15%">Subtotal</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                <?php
-                                $total = 0;
-                                $itemCounter = 1;
-                                while($itemCtrl>=$itemCounter){
-                                    if(isset($_GET['itemCtrl'])){
-                                        $item = $_GET['item'.$itemCounter];
-                                        $qty = $_GET['qty'.$itemCounter];
-                                        $price = $_GET['price'.$itemCounter];
-                                        $subTotal = (float)$qty*(float)$price;
-                                    }
-                                    else{
-                                        $item = NULL;
-                                        $qty = NULL;
-                                        $price = NULL;
-                                        $subTotal = NULL;
-                                    }
-
+                                    <?php
+                                        $barCodeTotal       = 0;
+                                        $itemBarCodeCounter = 0;
                                     ?>
+                                    <?php while($itemBarCodeCtrl > $itemBarCodeCounter): ?> 
+                                        <?php 
+                                            if(isset($_GET['itemBarCodeCtrl']))
+                                            {
+                                                $item     = $_GET['itemBarCode'.$itemBarCodeCounter];
+                                                $qty      = $_GET['qtyBarCode'.$itemBarCodeCounter];
+                                                $price    = $_GET['priceBarCode'.$itemBarCodeCounter];
+                                                $subTotal = (float)$qty*(float)$price;
+                                            }
+                                            else
+                                            {
+                                                $item     = NULL;
+                                                $qty      = NULL;
+                                                $price    = NULL;
+                                                $subTotal = NULL;
+                                            }
+                                        ?> 
                                     <tr>
                                         <td>
-                                            <select dir="rtl" class="form-control selectpicker" name="item<?php echo $itemCounter; ?>" data-live-search="true">
-                                                <?php
-                                                    $getItemForAdding = mysqli_query($mysqli, "SELECT * FROM inventory");
-                                                    while($newItemsForAdding=$getItemForAdding->fetch_assoc()){
-                                                ?>
-                                                        <option data-tokens="<?php echo strtoupper($newItemsForAdding['item_name']); ?>" class="" value="<?php echo $newItemsForAdding['id']; ?>" <?php if($item==$newItemsForAdding['id']){echo 'selected';} ?>>
-                                                            <?php echo strtoupper($newItemsForAdding['item_code'].' - '.$newItemsForAdding['item_name'].' - PHP'.$newItemsForAdding['item_price']); ?>
-                                                        </option>
-                                                <?php } ?>
-                                            </select>
+                                            <input type="text" class="form-control" name="itemBarCode<?=$itemBarCodeCounter?>" value="<?=$item?>">
                                         </td>
                                         <td>
-                                            <input type="number" class="form-control" name="qty<?php echo $itemCounter; ?>" value="<?php echo $qty; ?>" placeholder="1" >
+                                            <input type="number" class="form-control" name="qtyBarCode<?=$itemBarCodeCounter?>" value="<?=$qty;?>" placeholder="0" >
                                         </td>
                                         <td>
-                                            <input type="number" class="form-control" name="price<?php echo $itemCounter; ?>" value="<?php echo $price ?>" step="0.0001" placeholder="0.00" >
+                                            <input type="number" class="form-control" name="priceBarCode<?=$itemBarCodeCounter?>" value="<?=$price?>" step="0.0001" placeholder="0.00" readonly >
                                         </td>
-                                        <td><input class="form-control" name="subTotal" value="<?php echo $subTotal; ?>" readonly></td>
+                                        <td><input class="form-control" name="subTotal" value="<?=$subTotal?>" readonly></td>
                                     </tr>
-                                <?php
-                                    $itemCounter++;
-                                    $total += $subTotal;
-                                } ?>
-                                </tbody>
-                            </table>
-                            <input type="text" name="itemCtrl" value="<?php echo $itemCtrl; ?>" style="visibility: hidden;">
-                            <span class="float-right"><b>TOTAL: ₱<?php echo number_format($total,2); ?></b></span>
-                            <br>
-                            <button type="submit" class="btn btn-success btn-sm float-right" name="add_item">Add Item</button>
-                            <br>
-                            <br>
-                            <br>
-                            <table class="table" width="100%" cellspacing="0">
-                                <thead>
-                                <tr>
-                                    <th width="10%;">Control ID</th>
-                                    <th width="">Full Name</th>
-                                    <th width="">Address</th>
-                                    <th width="">Phone Number</th>
-                                    <th width="">Amount Paid</th>
-                                </tr>
-                                </thead>
-                                <tbody>
+                                    <?php
+                                        $itemBarCodeCounter++;
+                                        $barCodeTotal += $subTotal;
+                                    ?>
+                                    <?php endwhile ?> 
                                     <tr>
-                                        <td><input type="text" class="form-control" name="transactionID" value="<?php echo ++$lastTransactionID; ?>" readonly></td>
-                                        <td><input type="text" class="form-control" name="full_name" placeholder="ex: Juan Crus" value="Juan Cruz"></td>
                                         <td>
-                                            <textarea class="form-control" name="address" style="min-height: 100px;">Angeles City</textarea>
+                                            <input type="text" class="form-control" name="itemBarCode" value="">
                                         </td>
-                                        <td><input type="text" class="form-control" name="phone_num" placeholder="ex: 04876494843" value="09090912098"></td>
-                                        <td><input type="number" step="0.01" class="form-control" name="amount_paid" placeholder="<?php echo $total; ?>" value="<?php echo $total; ?>" required></td>
+                                        <td>
+                                            <input type="number" class="form-control" name="qtyBarCode" value="0" placeholder="0" >
+                                        </td>
+                                        <td>
+                                            <input type="number" class="form-control" name="priceBarCode" value="0" step="0.0001" placeholder="0.00" readonly >
+                                        </td>
+                                        <td><input class="form-control" name="subTotal" value="" readonly></td>
                                     </tr>
                                 </tbody>
                             </table>
-                            <br/>
-                            <center>
-                                <button class="btn btn-sm btn-primary m-1" name="save" type="submit"><i class="far fa-save" ></i> Save</button>
-                                <a href="transactions.php" class="btn btn-danger btn-sm m-1"><i class="fas as fa-sync"></i> Cancel</a>
-                            </center>
+                                     <input type="text" name="itemBarCodeCtrl" value="<?php echo $itemBarCodeCtrl; ?>" style="visibility: hidden;">                           
+                                     <span class="float-right"><b>TOTAL: ₱ <?=number_format($barCodeTotal,2)?></b></span>
+                                <br>
+                                <button type="submit" class="btn btn-success btn-sm float-right" name="add_item_barcode">Add Item</button>
+                                <br>
+                                <br>
+                                <br>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th width="45%">Item</th>
+                                            <th width="25%">Quantity</th>
+                                            <th width="15%">Adjusted Price / Item</th>
+                                            <th width="15%">Subtotal</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                    $total = 0;
+                                    $itemCounter = 1;
+                                    while($itemCtrl>=$itemCounter){
+                                        if(isset($_GET['itemCtrl'])){
+                                            $item = $_GET['item'.$itemCounter];
+                                            $qty = $_GET['qty'.$itemCounter];
+                                            $price = $_GET['price'.$itemCounter];
+                                            $subTotal = (float)$qty*(float)$price;
+                                        }
+                                        else{
+                                            $item = NULL;
+                                            $qty = NULL;
+                                            $price = NULL;
+                                            $subTotal = NULL;
+                                        }
+
+                                        ?>
+                                        <tr>
+                                            <td>
+                                                <select dir="rtl" class="form-control selectpicker" name="item<?php echo $itemCounter; ?>" data-live-search="true">
+                                                    <?php
+                                                        $getItemForAdding = mysqli_query($mysqli, "SELECT * FROM inventory");
+                                                        while($newItemsForAdding=$getItemForAdding->fetch_assoc()){
+                                                    ?>
+                                                            <option data-tokens="<?php echo strtoupper($newItemsForAdding['item_name']); ?>" class="" value="<?php echo $newItemsForAdding['id']; ?>" <?php if($item==$newItemsForAdding['id']){echo 'selected';} ?>>
+                                                                <?php echo strtoupper($newItemsForAdding['item_code'].' - '.$newItemsForAdding['item_name'].' - PHP '.$newItemsForAdding['item_price']); ?>
+                                                            </option>
+                                                    <?php } ?>
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <input type="number" class="form-control" name="qty<?php echo $itemCounter; ?>" value="<?php echo $qty; ?>" placeholder="1" >
+                                            </td>
+                                            <td>
+                                                <input type="number" class="form-control" name="price<?php echo $itemCounter; ?>" value="<?php echo $price ?>" step="0.0001" placeholder="0.00" >
+                                            </td>
+                                            <td><input class="form-control" name="subTotal" value="<?php echo $subTotal; ?>" readonly></td>
+                                        </tr>
+                                    <?php
+                                        $itemCounter++;
+                                        $total += $subTotal;
+                                    } ?>
+                                    </tbody>
+                                </table>
+                                <input type="text" name="itemCtrl" value="<?php echo $itemCtrl; ?>" style="visibility: hidden;">
+                                <span class="float-right"><b>TOTAL: ₱ <?php echo number_format($total,2); ?></b></span>
+                                <br>
+                                <button type="submit" class="btn btn-success btn-sm float-right" name="add_item">Add Item</button>
+                                <br>
+                                <br>
+                                <br>
+                                <table class="table" width="100%" cellspacing="0">
+                                    <thead>
+                                    <tr>
+                                        <th width="10%;">Control ID</th>
+                                        <th width="">Full Name</th>
+                                        <th width="">Address</th>
+                                        <th width="">Phone Number</th>
+                                        <th width="">Amount Paid</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><input type="text" class="form-control" name="transactionID" value="<?php echo ++$lastTransactionID; ?>" readonly></td>
+                                            <td><input type="text" class="form-control" name="full_name" placeholder="ex: Juan Crus" value="Juan Cruz"></td>
+                                            <td>
+                                                <textarea class="form-control" name="address" style="min-height: 100px;">Angeles City</textarea>
+                                            </td>
+                                            <td><input type="text" class="form-control" name="phone_num" placeholder="ex: 04876494843" value="09090912098"></td>
+                                            <td><input type="number" step="0.01" class="form-control" name="amount_paid" placeholder="<?php echo $total; ?>" value="<?php echo $total; ?>" required></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <br/>
+                                <center>
+                                    <button class="btn btn-sm btn-primary m-1" name="save" type="submit"><i class="far fa-save" ></i> Save</button>
+                                    <a href="transactions.php" class="btn btn-danger btn-sm m-1"><i class="fas as fa-sync"></i> Cancel</a>
+                                </center>
                         </form>
                     </div>
                 </div>
